de45acc96108b65c8373f780b2b416781f9908d6
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-11-01 18:50:50 +0100

    Add authentification feature

--- MyURLSaver/MyURLSaver/settings.py
+++ MyURLSaver/MyURLSaver/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'MyURLSaver_app.apps.MyurlsaverAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'MyURLSaver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'MyURLSaver_app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home'
--- MyURLSaver/MyURLSaver/urls.py
+++ MyURLSaver/MyURLSaver/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from  MyURLSaver_app import views as core_views
+from django.contrib.auth import views as auth_views
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
