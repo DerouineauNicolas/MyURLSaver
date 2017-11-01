b7360f5b0976280e4eacfbed68511645baf6527d
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-11-01 13:27:24 +0100

    Add first model

--- myURLSaver/myURLSaver/settings.py
+++ myURLSaver/myURLSaver/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myURLSaver_app.apps.MyurlsaverAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
