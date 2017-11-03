c74dce7d0060e15c16ea9818d5f1d861891352d5
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-11-03 15:02:01 +0100

    Set model for url

--- MyURLSaver/MyURLSaver_app/models.py
+++ MyURLSaver/MyURLSaver_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class URL(models.Model):
+    url = models.CharField(max_length=300)
+    date = models.DateField()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    URLs = models.ManyToManyField(URL)
+
+
+
