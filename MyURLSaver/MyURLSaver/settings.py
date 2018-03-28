af719981bceb0de9614807453c45f3cb6f563d40
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2018-03-28 13:48:56 +0200

    Use env var to store SECRET_KEY

--- MyURLSaver/MyURLSaver/settings.py
+++ MyURLSaver/MyURLSaver/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8!p%n@q@y(1jh^fy-i4srweb=(!q1gac_xlq1dxq_q#aa%di88'
+if("MyURLSaverProdKey" in os.environ):
+    SECRET_KEY = os.environ['MyURLSaverProdKey']
+else:
+    print('Error: MyURLSaverProdKey env var is undefined')
+    sys.exit()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
