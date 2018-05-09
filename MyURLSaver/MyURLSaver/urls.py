cb4fe42330df66a090457f57767b05ff40dc1dce
Nicolas DEROUINEAU <nicolas.derouineau@telecomparis-tech.fr>
2018-05-09 13:39:51 +0000

    Add url prefix pattern

--- MyURLSaver/MyURLSaver/urls.py
+++ MyURLSaver/MyURLSaver/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from  MyURLSaver_app import views as core_views
 from django.contrib.auth import views as auth_views
 
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+urls1 = [
     url(r'^$', core_views.home, name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
@@ -29,4 +27,14 @@ urlpatterns = [
     url(r'^addurls/$', core_views.addurls, name='addurls'),
     url(r'^delurls/$', core_views.delurls, name='delurls'),
     url(r'^seeurls/$', core_views.seeurls, name='seeurls'),
-]
+    url(r'^admin/', admin.site.urls),
+    ]
+   
+
+urlpatterns = [
+    url(r'^MyURLSaver/', include(urls1)),
+
+    ]
+
+
+
