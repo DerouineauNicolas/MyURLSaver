674cd9ab0c1b7e3e9e13005920a8aab29854c6c8
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2018-09-09 12:47:46 +0200

    Upgrade to django 2.0

--- MyURLSaver/MyURLSaver/urls.py
+++ MyURLSaver/MyURLSaver/urls.py
@@ -23,8 +23,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', core_views.home, name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^login/$',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    url(r'^logout/$',auth_views.LoginView.as_view(template_name="logout.html"), name="logout"),
     url(r'^signup/$', core_views.signup, name='signup'),
     url(r'^addurls/$', core_views.addurls, name='addurls'),
     url(r'^delurls/$', core_views.delurls, name='delurls'),
